7e1f2cd7aa02a166bc229d86e1e79b2e6abcc06e
Viktor <viktor.johansson96@gmail.com>
2019-06-02 20:04:52 +0200

    added things

--- nightwatch.conf.BASIC.js
+++ nightwatch.conf.BASIC.js
@@ -1,4 +1,4 @@
-require('env2')('.env'); // optionally store youre Evironment Variables in .env
+
 const seleniumServer = require("selenium-server");
 const chromedriver = require("chromedriver");
 const SCREENSHOT_PATH = "./screenshots/";
