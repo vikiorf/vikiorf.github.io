9903a503b1ef2696414726c7ee5ea105c0caa13a
Viktor <viktor.johansson96@gmail.com>
2019-06-07 17:14:17 +0200

    Added logo to contact

--- E2E/test/e2e/siteTest.js
+++ E2E/test/e2e/siteTest.js
@@ -3,20 +3,26 @@ var conf = require('../../nightwatch.conf.js');
 module.exports = {
   'Demo test GitHub': function (browser) {
     browser
-      .url('http://www.github.com/dwyl')   // visit the url
+      .url('https://vikiorf.github.io/')   // visit the url
       .waitForElementVisible('body'); // wait for the body to be rendered
-      // check if we are seeing the Mobile Version of GitHub
+      // check if we are seeing the Mobile Version of GitHub            
       browser.element('css selector', '.switch-to-desktop', function(result) {
         if(result.status != -1) { //Element exists, do something
           browser.click('.switch-to-desktop')
           .waitForElementVisible('body'); // wait for the body to be rendered
         }
       });
-    // part two:
+    // part two:    
     browser
-      .assert.containsText('body', 'dwyl.com') // assert body contains text
+      .assert.containsText('body', 'Home') // assert body contains text
       .timeouts("implicit",5000)
-      .saveScreenshot(conf.imgpath(browser) + 'dwyl.png')
+      .saveScreenshot(conf.imgpath(browser) + 'Home')
       .end();
     }
+    /*
+    //Part three
+    browser
+    .assert.title()
+    .end();
+    */
   };
