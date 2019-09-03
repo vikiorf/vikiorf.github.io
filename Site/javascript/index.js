715ae1e4c40220d4bd3a7091ff325b3f64586449
Viktor <viktor.johansson96@gmail.com>
2019-09-03 13:39:23 +0200

    Swapping text

--- Site/javascript/index.js
+++ Site/javascript/index.js
@@ -1,5 +1,14 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
 
-function textAppears(){
+/*function textAppears(){
     document.getElementById('textAppearss').innerHTML = "Test";
-}
+}*/
+
+function textAppears() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Hello") {
+      x.innerHTML = "Swapped text!";
+    } else {
+      x.innerHTML = "Hello";
+    }
+  }
